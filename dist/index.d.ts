c7e64076226034146c8786f8f94145200dfdb8d4
Martyn Janes <martyn@obany.com>
2018-03-08 16:17:39 +0300

    Initial Commit

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Combined index of all the modules.
+ */
+export * from "@iota-pico/core";
+export * from "@iota-pico/data";
+export * from "@iota-pico/api";
+export * from "@iota-pico/crypto";
+export * from "@iota-pico/business";
+export * from "@iota-pico/pal-browser";
+export { ProofOfWork as ProofOfWorkBox } from "@iota-pico/pow-box";
+export { ProofOfWork as ProofOfWorkJs } from "@iota-pico/pow-js";
+export { ProofOfWork as ProofOfWorkWebGl } from "@iota-pico/pow-webgl";
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const fs = require('fs');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const packageJson = require('./package.json');
+
+const pkgFolder = path.join(__dirname, './pkg');
+const bootstrapFile = path.join(pkgFolder, 'bootstrap.js');
+
+let isDir = false;
+try {
+    isDir = fs.lstatSync(pkgFolder).isDirectory();
+} catch (err) { }
+if (!isDir) {
+    fs.mkdirSync(pkgFolder);
+}
+fs.writeFileSync(bootstrapFile, "exports.default = require(\"../dist/index\");");
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const plugins = [];
+
+if (isProd) {
+    plugins.push(new UglifyJsPlugin());
+}
+
+module.exports = {
+    entry: bootstrapFile,
+    output: {
+        path: path.resolve(__dirname, './pkg'),
+        filename: "iota-pico-lib-browser" + (isProd ? '.min' : '') + '.js',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+        library: "IotaPico",
+        umdNamedDefine: true
+    },
+    externals: [],
+    devtool: isProd ? undefined : "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        "presets": [
+                            "@babel/preset-env"
+                        ],
+                        "plugins": [
+                            [
+                                "@babel/plugin-transform-runtime",
+                                {
+                                    "helpers": false,
+                                    "polyfill": false,
+                                    "regenerator": true
+                                }
+                            ],
+                            [
+                                "babel-plugin-transform-builtin-extend", 
+                                {
+                                    "globals": ["Error"]
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins,
+    node: {
+        fs: "empty"
+    }
+};
