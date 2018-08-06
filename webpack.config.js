b4e3c279d7ef5fad7b867b420ff82dd8bb2b4773
Martyn Janes <martyn@obany.com>
2018-08-06 08:12:41 +0300

    Updated Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,6 @@ module.exports = {
                                 "@babel/plugin-transform-runtime",
                                 {
                                     "helpers": false,
-                                    "polyfill": false,
                                     "regenerator": true
                                 }
                             ],
